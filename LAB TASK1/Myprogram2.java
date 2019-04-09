991784727f71d558987d552cd154e8790ecac5f9
Dua-Ali <49425527+Dua-Ali@users.noreply.github.com>
2019-04-09 19:31:21 +0500

    Add files via upload

---
+++ LAB TASK1/Myprogram2.java
@@ -0,0 +1,21 @@
+class Myprogram2{
+public static void main(String[] args){
+	String name="Dua";
+	double marks=90;
+	float per=95.5f;
+	char grade='A';
+	boolean status=true;
+	char p='%';
+	System.out.println("Name:"+name);
+	System.out.println("Marks:"+marks);
+	System.out.println("percentage:"+per+p);
+	System.out.println("Grade:"+grade);
+	System.out.println("Status:pass"+status);
+	
+	
+	
+	
+	
+
+}
+}
